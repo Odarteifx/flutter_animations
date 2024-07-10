0247c6f8f8d4851d7dccdb6ecadc55f9f5405ca9
william lamptey <williamlamp512@gmail.com>
2024-07-10 20:47:27 +0000

    Explicit Animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -40,8 +41,14 @@ class _MyHomePageState extends State<MyHomePage> {
           backgroundColor: Theme.of(context).colorScheme.inversePrimary,
           title: Text(widget.title),
         ),
-        body: Column(
-          children: [Bookmark()],
+        body: const Column(
+          children: [
+            Bookmark(),
+            SizedBox(
+              height: 50,
+            ),
+            ExplicitAnimation(),
+          ],
         ));
   }
 }
@@ -53,32 +60,115 @@ class Bookmark extends StatefulWidget {
   State<Bookmark> createState() => _BookmarkState();
 }
 
-
 class _BookmarkState extends State<Bookmark> {
   bool _change = false;
 
-void _bookmarked(){
-  setState(() {
-    _change = !_change;
-  });
-}
+  void _bookmarked() {
+    setState(() {
+      _change = !_change;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return AnimatedRotation(
-        turns: _change? 1 : 0,
-        curve: Curves.decelerate,
-        duration: const Duration(seconds: 1),
-        child: FloatingActionButton(
-          backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
-          elevation: 0,
-          shape: const CircleBorder(),
-          onPressed: () {
-            _bookmarked();
-          },
-          child: Icon(
-            _change? Icons.star : Icons.star_outline,
-            color:  Colors.grey ,
+    return Stack(
+      children: [
+        AnimatedRotation(
+            turns: _change ? 1 : 0,
+            curve: Curves.decelerate,
+            duration: const Duration(milliseconds: 500),
+            child: FloatingActionButton(
+              backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
+              elevation: 0,
+              shape: const CircleBorder(),
+              onPressed: () {
+                _bookmarked();
+              },
+              child: Icon(
+                _change ? Icons.star : Icons.star_outline,
+                color: Colors.grey,
+              ),
+            )),
+      ],
+    );
+  }
+}
+
+class ExplicitAnimation extends StatefulWidget {
+  const ExplicitAnimation({super.key});
+
+  @override
+  State<ExplicitAnimation> createState() => _ExplicitAnimationState();
+}
+
+class _ExplicitAnimationState extends State<ExplicitAnimation>
+    with TickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 15),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Align(
+          alignment: Alignment.center,
+          child: RotationTransition(
+            turns: _animationController,
+            child: Container(
+              height: 150,
+              width: 150,
+              decoration: const BoxDecoration(color: Colors.orange),
             ),
-        ));
+          ),
+        ),
+        Align(
+         
+          child: TimeStopper(
+            controller: _animationController,
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+}
+
+class TimeStopper extends StatelessWidget {
+  final AnimationController controller;
+  const TimeStopper({super.key, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (controller.isAnimating) {
+          controller.stop();
+        } else {
+          controller.repeat();
+        }
+      },
+      child: Container(
+        decoration: const BoxDecoration(
+          color: Colors.transparent
+        ),
+        width: 60,
+        height: 50,
+        child: const Text('Control'),
+      ),
+    );
   }
 }
